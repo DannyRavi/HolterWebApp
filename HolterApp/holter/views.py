3c9d0004ce659d0d30c07e8081ea7ffdf48caab6
Server <Server@SERVER>
2019-06-24 14:12:14 +0430

    add default to model

--- HolterApp/holter/views.py
+++ HolterApp/holter/views.py
@@ -4,9 +4,10 @@ from .serializers import ProfileSerializerV1
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
-
+# @csrf_exempt
 class ProfileView(generics.ListCreateAPIView):
     serializer_class = ProfileSerializerV1
     queryset = Profiles.objects.all()
