f0862fb26fce700c6be8d325a1ef77d8b3fa2e4a
DANNY <danykhosravi@gmail.com>
2019-06-24 14:12:14 +0430

    add default to model

--- HolterApp/holter/models.py
+++ HolterApp/holter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.forms import ModelForm
+from datetime import datetime
 
 TITLE_CHOICES = [
     ('MR', 'Mr.'),
@@ -8,10 +9,10 @@ TITLE_CHOICES = [
 
 # Create your models here.
 class Profiles(models.Model):
-    nation_code = models.CharField(max_length=11)
-    phone_number = models.CharField(max_length=11)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    birth_day = models.DateTimeField()
-    address = models.CharField(max_length=120)
-    sex = models.CharField(max_length=3, choices=TITLE_CHOICES)
+    nation_code = models.CharField(max_length=10, blank=True, default='1234567890')
+    phone_number = models.CharField(max_length=11, blank=True, default='')
+    first_name = models.CharField(max_length=20, blank=True, default='')
+    last_name = models.CharField(max_length=20, blank=True, default='')
+    birth_day = models.DateTimeField(default=datetime.now, blank=True)
+    address = models.CharField(max_length=120, blank=True, default='')
+    sex = models.CharField(max_length=3, choices=TITLE_CHOICES,default='MR')
