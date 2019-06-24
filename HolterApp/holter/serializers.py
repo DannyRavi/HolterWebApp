7926ac873b3419b6bf3f8478711f9d8456e3da81
DANNY <danykhosravi@gmail.com>
2019-06-24 14:02:43 +0430

    simple api lunched

--- HolterApp/holter/serializers.py
+++ HolterApp/holter/serializers.py
@@ -5,3 +5,7 @@ class ProfileSerializerV1(serializers.ModelSerializer):
     class Meta:
         model = Profiles
         fields = "__all__"
+
+
+    def create(self, validated_data):
+        return Profiles.objects.create(**validated_data)    
--- HolterApp/holter/views.py
+++ HolterApp/holter/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Profiles
+from .serializers import ProfileSerializerV1
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+
+class ProfileView(generics.ListCreateAPIView):
+    serializer_class = ProfileSerializerV1
+    queryset = Profiles.objects.all()
