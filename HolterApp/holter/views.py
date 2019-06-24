27e7d44996452a9a9bec9edb0258c4e74a098388
Server <Server@SERVER>
2019-06-24 14:43:34 +0430

    post method done

--- HolterApp/holter/views.py
+++ HolterApp/holter/views.py
@@ -4,10 +4,14 @@ from .serializers import ProfileSerializerV1
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import mixins
 # Create your views here.
 
 # @csrf_exempt
 class ProfileView(generics.ListCreateAPIView):
     serializer_class = ProfileSerializerV1
     queryset = Profiles.objects.all()
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profiles.objects.all()
+    serializer_class = ProfileSerializerV1    
