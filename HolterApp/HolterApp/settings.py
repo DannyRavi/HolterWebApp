917933eeb60d7d661b09ac4f7dc257c315b71599
DANNY <danykhosravi@gmail.com>
2019-06-25 13:33:15 +0430

    authenticate is added

--- HolterApp/HolterApp/settings.py
+++ HolterApp/HolterApp/settings.py
@@ -51,6 +51,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
 ROOT_URLCONF = 'HolterApp.urls'
 
 TEMPLATES = [
