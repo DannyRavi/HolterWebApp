750e7ffa1fa2976df1ac30b88cabbe78f952613b
Server <Server@SERVER>
2019-06-26 17:09:26 +0430

    send to github

--- HolterApp/holter/serializers.py
+++ HolterApp/holter/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
 from .models import Profiles
+from doc.models import Docs
+from doc.serializers import DocsSerializer
 
 class ProfileSerializerV1(serializers.ModelSerializer):
+
+    # docs = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # doc =  DocsSerializer(write_only=True, required=False)
+    # doc =  DocsSerializer(many=True, read_only=True)
+    # user =  serializers.IntegerField(write_only=True)
     class Meta:
         model = Profiles
         fields = "__all__"
 
-
+    
     def create(self, validated_data):
-        return Profiles.objects.create(**validated_data)    
+        # doc = validated_data.pop('docs')
+        profile = Profiles.objects.create(**validated_data)
+        # my_doc = Docs.objects.create(**doc,author=profile)
+        return  profile
 
 
