2b275cd3d5a6af716f508bd220e081a8de322dc0
Server <Server@SERVER>
2019-07-06 14:55:30 +0430

    almost completet patient jasons

--- HolterApp/holter/serializers.py
+++ HolterApp/holter/serializers.py
@@ -36,3 +36,28 @@ class ProfileSerializerV1(serializers.ModelSerializer):
         return profile
 
 
+class PaitentApp(serializers.ModelSerializer):
+
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    post = DocsSerializer(write_only=True, required=False)
+    user = serializers.IntegerField(write_only=True)
+    class Meta:
+        model = Profiles
+    mobile_number = models.CharField(max_length=11, blank=True, default='')
+         fields = ('nation_code', 'first_name', 'last_name', 'phone_number','age','height','weigth',
+            'address','postal_code','email','gender')
+
+    
+    def create(self, validated_data):
+        if 'doc' in validated_data:
+            user = validated_data.pop('user')
+            user = get_object_or_404(User, pk=user)
+            post = validated_data.pop('doc')
+            profile = Profiles.objects.create(user=user, **validated_data)
+            my_post = Docs.objects.create(**post, author=profile)
+        else:
+            user = validated_data.pop('user')
+            user = get_object_or_404(User, pk=user)
+            profile = Profiles.objects.create(user=user, **validated_data)
+
+        return profile
--- HolterApp/holter/urls.py
+++ HolterApp/holter/urls.py
@@ -6,5 +6,7 @@ app_name = "holter"
 urlpatterns = [
     path('', views.ProfileView.as_view(), name="profile_list"),
     path('<int:pk>/', views.ProfileDetail.as_view(), name="profile_retrive"),
-    path('0813595858/', views.ProfileDetail.as_view(), name="profile_retrive"),
+    path('0813595858/?$', views.ProfileDetail.as_view(), name="profile_retrive"),
+    path('patient/', views.ProfileViewPatientApp.as_view(), name="ProfileViewPatientApp_list"),
+    path('patient/<int:pk>/?$', views.ProfileDetailPatientApp.as_view(), name="ProfileDetailPatientApp_retrive"),
 ]
