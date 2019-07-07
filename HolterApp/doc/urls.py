81b38fe8237bd6741132c71090bbf79d11e3afbf
DANNY <danykhosravi@gmail.com>
2019-07-07 09:37:45 +0430

    ready to use1

--- HolterApp/doc/urls.py
+++ HolterApp/doc/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "doc"
 
 urlpatterns = [
-    path('', views.DocView.as_view(), name="profile_list"),
-    path('<int:pk>/', views.DocDetail.as_view(), name="profile_retrive"),
-    path('0813595858/', views.DocDetail.as_view(), name="profile_retrive"),
+     path('docers/', views.DocView.as_view(), name="profile_list"),
+    # path('<int:pk>/', views.DocDetail.as_view(), name="profile_retrive"),
+    # path('0813595858/', views.DocDetail.as_view(), name="profile_retrive"),
 ]
